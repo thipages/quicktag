743eace875ab1845bc10419b4cd0ce380c557394
thipages <thipages@gmail.com>
2020-04-12 02:34:13 +0200

    single file update

--- src/thipages/quick/QTag.php
+++ src/thipages/quick/QTag.php
@@ -44,15 +44,3 @@ class QTag {
         ]));
     }
 }
-/*
- <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Title</title>
-</head>
-<body>
-
-</body>
-</html>
- */
